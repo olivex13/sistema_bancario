6cb49543284915bbe6a49ba326418938ffb9aa35
Douglas Silva <silva.o.douglas@gmail.com>
2023-12-20 19:36:10 -0300

    readme + ajuste das funções do menu

---
+++ readme.md
@@ -0,0 +1,28 @@
+#          Sistema Bancário 👨‍💻
+
+## Objetivo:
+
+Este projeto foi desenvolvido com objetivos acadêmicos, como parte do meu curso de formação em Python da DIO. Ele serve como uma aplicação prática dos conceitos aprendidos durante a minha formação.
+
+
+## Condições do programa:
+
+### Depósito:
+
+- Deve ser possível depositar valores positivos.
+- Todos os depósitos devem ser armazenados no extrato.
+
+### Saque:
+
+- Só pode realizar 3 saques diários.
+- Limite máximo de saque R$ 500,00
+- Se não tiver saldo exibir mensagem informando que não tem saldo.
+- Os saques devem ser armazenados no extrato.
+
+### Extrato:
+
+- Exibir as transações realizadas.
+- Se não tiver movimentações, aparecer a mensagem informando que não teve transações.
+- Formato de exibição dos valores deve ser R$ XXX.XX.
+
+
--- sistema_bancario.py
+++ sistema_bancario.py
@@ -1,26 +1,52 @@
 print ("SISTEMA BANCÁRIO DO BANCO LARANJA".center(60, " "))
 print ("")
-saldo_conta = 500
+saldo_conta = 0
+extrato = " "
+numero_saques = 0
+LIMITE_SAQUES = 3
+LIMITE_VALOR = 500
 selecionar_funcao = None
 
-while selecionar_funcao !=0:
-    selecionar_funcao = int(input("[1] Sacar\n[2] Depositar\n[3] Consultar Saldo\n[0] Sair\n \n            Digite a opção desejada: "))
 
-    if selecionar_funcao == 1:
-        valor_saque = int(input("Digite o valor do saque: "))
-        if valor_saque <= saldo_conta:
-                saldo_conta -= valor_saque
-                print(f"Saque realizado com sucesso, o valor do saldo é {saldo_conta}")
-        else:
-                print("Saldo insuficiente. Operação de saque cancelada.")
+menu = """
+[1] Sacar
+[2] Depositar
+[3] Extrato 
+[0] Sair
+"""
 
-    elif selecionar_funcao == 2:
-        valor_deposito = int(input("Digite o valor do deposito: "))
-        saldo_conta += valor_deposito
-        print(f"Deposito realizado com sucesso, seu novo saldo é {saldo_conta}")
+while True:
+    selecionar_funcao = input(menu)
+
+    if selecionar_funcao == '1':
+        valor_saque = float(input("Digite o valor do saque: "))
+        excedeu_limite = valor_saque > LIMITE_VALOR
+        excedeu_saques = numero_saques > LIMITE_SAQUES
+        excedeu_saldo = valor_saque > saldo_conta
         
-    elif selecionar_funcao == 3:
-        print(f"O seu saldo é {saldo_conta}")
+        if excedeu_limite:
+            print("Operação falhou! O valor do saque excede o limite.")
+        elif excedeu_saques:
+            print("Operação falhou! O valor do saque excede o limite de saques.")
+        elif excedeu_saldo:
+            print("Operação falhou! O valor do saque excede o Saldo.")
+        elif valor_saque > 0:
+            saldo_conta -= valor_saque
+            numero_saques += 1
+            print(f'Saque realizado com sucesso, seu novo saldo é R$ {saldo_conta:.2f}')
+            extrato += f"Saque: R${valor_saque:.2f} \n"
+           
+    elif selecionar_funcao == '2':
+        valor_deposito = float(input("Digite o valor do deposito: "))
+        if valor_deposito > 0:
+            saldo_conta += valor_deposito
+            extrato += f"Deposito: R${valor_deposito:.2f} \n"
+            print(f"Valor depositado com sucesso, seu novo saldo é: R$ {saldo_conta:.2f}")
+        else:
+            print("Operação falhou! Digite um valor válido.")
+        
+    elif selecionar_funcao == '3':
+        print(f"O extrato da sua conta é: \n {extrato} O valor em conta é: {saldo_conta:.2f}")
 
     elif selecionar_funcao == 0:
         print("Obrigado por usar nossos serviços, até breve.")
